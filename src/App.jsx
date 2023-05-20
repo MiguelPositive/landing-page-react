68a9f04524e0661ca28ba6f039863a35be251136
miguel angel clavijo moreno <miguelpositive@gmail.com>
2023-05-20 14:37:35 -0500

    animaciones del menu añadidas

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import Context from "./components/context/Context";
+import Header from "./components/header/Header";
 function App() {
-  return <></>;
+  return (
+    <Context>
+      <div className="pl-5 pr-5 mt-3">
+        <Header />
+      </div>
+    </Context>
+  );
 }
 
 export default App;
