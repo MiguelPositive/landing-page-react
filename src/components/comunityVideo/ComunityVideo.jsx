5733753c89f79ff9be2801a9012d50bc3435483f
miguel angel clavijo moreno <miguelpositive@gmail.com>
2023-06-03 18:16:22 -0500

    cambio el video

--- src/components/comunityVideo/ComunityVideo.jsx
+++ src/components/comunityVideo/ComunityVideo.jsx
@@ -8,22 +8,21 @@ const ComunityVideo = () => {
     <div
       className={`flex justify-center items-center flex-wrap-reverse 900px:flex-nowrap 900px:gap-10 ${activeBlur}`}
     >
-      <div id="container-video" className="w-full h-[20rem] 900px:w-[60%]">
-        <iframe
-          className="h-full w-full rounded-md mt-5 900px:mt-0"
-          width="560"
-          height="315"
-          src="https://www.youtube.com/embed/4FgnPwdgWAQ"
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-          allowfullscreen
-        ></iframe>
+      <div
+        id="container-video"
+        className="w-full h-full 900px:w-[60%] flex justify-center rounded-md overflow-hidden"
+      >
+        <div
+          className="fb-video rounded-md"
+          data-href="https://www.facebook.com/BarrioLaJoyaSomosTodos/videos/2108908209301779/"
+          data-width="500"
+          data-show-text="false"
+        ></div>
       </div>
 
       <div id="text-video" className="900px:w-[40%]">
         <p className="text-center mb-2 text-2xl font-bold">Titulo del video</p>
-        <p className="text-justify">
+        <p className="text-justify mb-5">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ex vel
           ratione fugit nisi, cupiditate ullam cumque sit id, debitis vero
           placeat architecto officia aperiam inventore quisquam nobis quis rerum
